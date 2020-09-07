937ca06e0cc1c1655249a4640cb6ed9346d5ed00
Fanz <fun_kidd@foxmail.com>
2020-09-07 15:57:25 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.less';
+
+// import style from './App.less';
+// console.log(style)
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React-ts
+      </header>
+    </div>
+  );
+}
+
+export default App;
