80e463a65abb0153e03941d2e076d29264831c80
Fanz <fun_kidd@foxmail.com>
2020-09-22 17:28:33 +0800

    build: update dotfiles & init demo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import './App.less';
+import React, { memo } from 'react';
+import { hot } from 'react-hot-loader/root';
 
-// import style from './App.less';
-// console.log(style)
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        React-ts
-      </header>
-    </div>
-  );
+interface CounterProps {
+    initialCount?: number;
 }
 
-export default App;
+const Counter = memo(function Counter({ initialCount = 0 }: CounterProps) {
+    const [count, setCount] = React.useState(initialCount);
+
+    const add = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div className="counter">
+            <input type="text" value={count} readOnly={false} />
+            <button type="button" onClick={add}>
+                +
+            </button>
+        </div>
+    );
+});
+
+function App() {
+    return (
+        <div className="app">
+            <Counter />
+        </div>
+    );
+}
+
+export default hot(App);
